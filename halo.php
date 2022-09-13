f361750cfdeebf082f49bb4f747ea98757cf0aaa
adiib123 <adiib0906@gmail.com>
2022-09-13 11:24:39 +0700

    halo lagi

--- halo.php
+++ halo.php
@@ -1,4 +1,4 @@
 <?
 echo "mencoba ini ku edit udah halo aku menocba edit di github";
-echo "mencoba ini ku edit udah halo aku menocba edit di github"
+echo "mencoba ini ku edit udah halo aku menocba edit di vsc"
 ?>
